d0089038151f44832889b67fb1805cee3c4ae021
anthrofax <afridhoikhsan@gmail.com>
2023-10-08 07:18:25 +0700

    Section 6 completely learned

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝 FAR AWAY 💼</h1>;
+}
+function Form() {
+  const [desc, setDesc] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+ function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Apa yang kamu butuhkan untuk perjalanan mu?</h3>
+
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." value={desc} onChange={(e) => setDesc(e.target.value)}/>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>Kamu memiliki 4 barang dalam daftar mu, dan kamu sudah mengemas X (X%)</em>
+    </footer>
+  );
+}
+
+export default App;
