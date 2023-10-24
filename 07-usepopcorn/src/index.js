bb17f87bdad8aeddb498b412705213da1f19432f
anthrofax <afridhoikhsan@gmail.com>
2023-10-24 23:22:27 +0700

    Building star rating logic completed

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Star from './Star'
-import './index.css'
+// import './index.css'
+import StarRating from './StarRating.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Star maxRating={5}/>
-    <Star maxRating={10}/>
-    <Star />
+    <StarRating maxRating={5}/>
   </React.StrictMode>
 );
 
