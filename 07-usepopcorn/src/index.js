a65dfd2adca6a1a71691edbe97e448a28a77251e
anthrofax <afridhoikhsan@gmail.com>
2023-11-17 21:32:22 +0700

    Set up the file for "Effect & Data Fetching" learning material section

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-// import './index.css'
-import StarRating from './StarRating.jsx';
+import App from './App-v2';
+import './index.css'
+// import StarRating from './StarRating.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <App />
+    {/* <StarRating maxRating={5}/> */}
   </React.StrictMode>
 );
 
