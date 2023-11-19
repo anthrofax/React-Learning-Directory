9a6e6bdd6ce04b4db2d9010c91cff7133a6bece2
anthrofax <afridhoikhsan@gmail.com>
2023-11-19 11:53:48 +0700

    Using an async function, adding a loading state & Handling error lecture completed

--- 07-usepopcorn/src/App-v2.js
+++ 07-usepopcorn/src/App-v2.js
@@ -1,32 +1,12 @@
 import { useState, useEffect } from "react";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
 const tempWatchedData = [
   {
     imdbID: "tt1375666",
     Title: "Inception",
     Year: "2010",
-    Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
     runtime: 148,
     imdbRating: 8.8,
     userRating: 10,
@@ -35,30 +15,49 @@ const tempWatchedData = [
     imdbID: "tt0088763",
     Title: "Back to the Future",
     Year: "1985",
-    Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
     runtime: 116,
     imdbRating: 8.5,
     userRating: 9,
   },
 ];
 
-const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
   const API_KEY = "4c00399c";
-  const query = 'interstellar';
-  const [movies, setMovies] = useState(tempMovieData);
+  const query = "avenger";
+  const [movies, setMovies] = useState([]);
+  // eslint-disable-next-line
   const [watched, setWatched] = useState(tempWatchedData);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(function () {
     async function fetchMovies() {
       try {
-        const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${query}`);
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${query}`
+        );
+
         const data = await res.json();
+
+        if (data.Response === "False")
+          throw new Error("Movie yang anda cari tidak ditemukan!");
 
         setMovies(data.Search);
       } catch (err) {
-        console.log(err.message);
+        setError(
+          err.message === "Failed to fetch"
+            ? "Koneksi anda terputus!"
+            : err.message
+        );
+      } finally {
+        setIsLoading(false);
       }
     }
 
@@ -73,7 +72,10 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {/* {isLoading ? <LoadingComponent /> : <MovieList movies={movies} />} */}
+          {isLoading && <LoadingComponent />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
         </Box>
         <Box>
           <>
@@ -85,6 +87,14 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function LoadingComponent() {
+  return <p className="loader">Loading...</p>;
+}
+
+function ErrorMessage({ message }) {
+  return <p className="error">⛔️ {message}</p>;
 }
 
 function NavBar({ children }) {
@@ -108,7 +118,15 @@ function Logo() {
 function Search() {
   const [query, setQuery] = useState("");
 
-  return <input className="search" type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />;
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
 }
 
 function NumResult({ movieNum }) {
