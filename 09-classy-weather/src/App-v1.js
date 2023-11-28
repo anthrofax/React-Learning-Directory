b8ad1a0ae4b1fb792b6748b0a74909fdd75ab413
anthrofax <afridhoikhsan@gmail.com>
2023-11-29 06:49:49 +0700

    Removing Boilerplate Code With Class Fields, Handling Communication Between Parent & Child, & Implementing Lifecycle Method to the project. --Classy Weather Project Finished

--- 09-classy-weather/src/App-v1.js
+++ 09-classy-weather/src/App-v1.js
@@ -33,21 +33,33 @@ function formatDay(dateStr) {
 }
 
 class App extends React.Component {
+  state = {
+    location: '',
+    isLoading: false,
+    displayLocation: '',
+    weather: {},
+  };
+
   constructor(props) {
     super(props);
-
-    this.state = {
-      location: 'Lisbon',
-      isLoading: false,
-      displayLocation: '',
-      weather: {},
-    };
 
     this.fetchWeather = this.fetchWeather.bind(this);
   }
 
-  async fetchWeather() {
+  componentDidMount() {
+    this.setState({location: localStorage.getItem('location') || ''});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) this.fetchWeather();
+
+    localStorage.setItem('location', this.state.location);
+  }
+
+  fetchWeather = async () => {
     try {
+      if (this.state.location.length < 2) return this.setState({weather: {}});
+      
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
       const geoRes = await fetch(
@@ -73,24 +85,24 @@ class App extends React.Component {
       this.setState({ weather: weatherData.daily });
       console.log(weatherData.daily);
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
+  };
+
+  setLocation = (e) => {
+    this.setState({location: e.target.value})
   }
 
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <div>
-          <input
-            type="text"
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          ></input>
-        </div>
-        <button onClick={this.fetchWeather}>Get Weather</button>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -107,7 +119,25 @@ class App extends React.Component {
 
 export default App;
 
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        ></input>
+      </div>
+    );
+  }
+}
+
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log('Weather Component Unmounted!');
+  }
+
   render() {
     const {
       temperature_2m_max: max,
