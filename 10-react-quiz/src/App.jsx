415ef10ff3c574ac42268df63a5debe933d12b85
anthrofax <afridhoikhsan@gmail.com>
2023-12-25 17:03:35 +0700

    Make loading question from fake API; Handling loading, error, and ready status; Handling situation when user start quiz

--- 10-react-quiz/src/App.jsx
+++ 10-react-quiz/src/App.jsx
@@ -1,9 +1,50 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Loader from "./Loader";
+import Error from './Error';
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialValue = {
+  questions: [],
+  status: 'loading',
+}
+
+const reducer = function(state, action) {
+  switch(action.type) {
+    case "dataRecieved": 
+      return {...state, questions: action.payload, status: "ready"};
+    case "dataFailed": 
+      return {...state, status: "error"};
+    case "start": 
+      return {...state, status: "active"};
+    default: 
+      throw new Error("Unknown action");
+  }
+}
 
 function App() {
+  const [{questions, status}, dispatch] = useReducer(reducer, initialValue);
+  const jumlahSoal = questions.length;
+
+  useEffect(function() {
+     fetch("http://localhost:8000/questions")
+    .then(res => res.json())
+    .then(questions => dispatch({type: "dataRecieved", payload: questions}))
+    .catch(err => dispatch({type:'dataFailed'}));
+  }, []);
+
   return (
-    <div className="App">
-      <DateCounter/>
+    <div className="app">
+      <Header/>
+      <Main>
+        {status === "loading" && <Loader/>}
+        {status === "error" && <Error/>}
+        {status === "ready" && <StartScreen jumlahSoal={jumlahSoal} dispatch={dispatch}/>}
+        {status === "active" && <Question/>}
+      </Main>
     </div>
   );
 }
---
+++ 10-react-quiz/src/Question.jsx
@@ -0,0 +1,9 @@
+function Question() {
+    return (
+        <div>
+            <p>Question 1/20</p>
+        </div>
+    )
+}
+
+export default Question
