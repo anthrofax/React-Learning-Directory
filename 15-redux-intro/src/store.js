1f19ea71c7a52181f05204390146a0255146aa83
anthrofax <afridhoikhsan@gmail.com>
2024-01-04 07:27:13 +0700

    Set up a Reducer for Classic Redux Environment

---
+++ 15-redux-intro/src/store.js
@@ -0,0 +1,23 @@
+import {createStore} from 'redux'
+
+const initialValue = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: ""
+}
+
+function reducer(state = initialValue, action) {
+    switch (action.type) {
+        case 'account/deposit':
+            return {...state, balance: state.balance + action.payload}
+        case 'account/withdraw':
+            return {...state, balance: state.balance - action.payload}
+        case 'account/requestLoan':
+            return {...state, balance: state.balance + action.payload.loan, loan: state.loan + action.payload.loan, loanPurpose: action.payload.loanPurpose}
+        case 'account/payLoan':
+            return {...state, balance: state.balance - state.loan, loan: 0};
+        default:
+            return state;
+    }
+}
+
